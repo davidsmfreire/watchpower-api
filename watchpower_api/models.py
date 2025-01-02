01a395737ea1e0f69d35fa544fe1695e759b8d8c
davidsmfreire <davidsmfreire@gmail.com>
2025-01-02 11:16:23 +0000

    chore: support pydantic v1 and v2

--- watchpower_api/models.py
+++ watchpower_api/models.py
@@ -1,12 +1,22 @@
+from typing import Optional, cast
 from pydantic import BaseModel, Field
 
+from pydantic.version import VERSION as _PYDANTIC_VERSION
+
+PYDANTIC_VERSION: str = str(_PYDANTIC_VERSION)
+
+if PYDANTIC_VERSION.startswith("2."):
+    from pydantic import ConfigDict
 
 class DeviceIdentifiers(BaseModel):
-    device_alias: str | None = Field(alias="devalias")
-    serial_number: str = Field(alias="sn")
-    wifi_pin: str = Field(alias="pn")
-    device_address: int = Field(alias="devaddr")
-    device_code: int = Field(alias="devcode")
+    device_alias: Optional[str] = Field(default=None, alias="devalias")
+    serial_number: str = Field(..., alias="sn")
+    wifi_pin: str = Field(..., alias="pn")
+    device_address: int = Field(..., alias="devaddr")
+    device_code: int = Field(..., alias="devcode")
 
-    class Config:
-        allow_population_by_field_name = True
+    if PYDANTIC_VERSION.startswith("2."):
+        model_config = ConfigDict(populate_by_name=True)
+    else:
+        class Config:
+            allow_population_by_field_name = True
